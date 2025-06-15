e7e0f60ca6913dca5fde5584239f6090d2ecb13b
Carlos <carlospelinski03@gmail.com>
2025-06-15 20:34:05 -0300

    Refactor routing and integrate React Query client in main application

--- Fronts/ControlPanel/src/App.tsx
+++ Fronts/ControlPanel/src/App.tsx
@@ -4,11 +4,13 @@ import "@mantine/core/styles.css"
 import Dashboard from "./Pages/Dashboard/Dashboard"
 import Auth from "./Pages/Auth/Auth"
 import Layout from "./components/Layout"
+import Business from "./Pages/Business/Business"
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout/>}>
-        <Route path="dashboard" element={<Dashboard/>}/>
+        <Route index element={<Dashboard/>}/>
+        <Route path="business" element={<Business/>}/>
       </Route>
       <Route path="/authentication" element={<Auth/>}/>
     </Routes>
--- Fronts/ControlPanel/src/main.tsx
+++ Fronts/ControlPanel/src/main.tsx
@@ -5,15 +5,21 @@ import { BrowserRouter } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
 import { Notifications } from '@mantine/notifications'
 import { AppContextProvider } from './Context/AppContext.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import "@mantine/notifications/styles.css"
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AppContextProvider>
         <MantineProvider>
           <Notifications />
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         </MantineProvider>
       </AppContextProvider>
     </BrowserRouter>
