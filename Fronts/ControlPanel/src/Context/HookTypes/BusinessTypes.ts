0994b896d43176a42f91d17d8555d93f219ecd26
Carlos <carlospelinski03@gmail.com>
2025-06-16 14:40:41 -0300

    Add BusinessFormData and BusinessData interfaces for business management

---
+++ Fronts/ControlPanel/src/Context/HookTypes/BusinessTypes.ts
@@ -0,0 +1,34 @@
+export interface BusinessFormData {
+  business_name: string;
+  business_description: string;
+  business_address1: string;
+  countryCode: string;
+  regionCode: string;
+  city: string;
+  business_phone: string;
+  business_email: string;
+}
+
+interface BusinessGeodata{
+  address1: string,
+  country: {
+    code : string;
+    label: string;
+  },
+  region: {
+    code: string;
+    label: string;
+  },
+  city:{
+    name: string;
+    label: string;
+  }
+}
+
+export interface BusinessData{
+  business_name: string;
+  business_description: string;
+  business_geodata: BusinessGeodata;
+  business_phone: string;
+  business_email: string;
+}
--- Fronts/ControlPanel/src/Context/HookTypes/ModalTypes.ts
+++ Fronts/ControlPanel/src/Context/HookTypes/ModalTypes.ts
@@ -4,14 +4,4 @@ export interface BusinessModal{
     businessId?: string;
 }
 
-export interface BusinessFormData{
-    business_name: string;
-    business_description?: string;
-    business_address1: string;
-    business_address2?: string;
-    business_country: string;
-    business_city: string;
-    business_state: string;
-    business_phone: string;
-    business_email: string;
-}
+
