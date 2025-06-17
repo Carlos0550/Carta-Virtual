0d1bf24f639e43f51b89eacb56973a9557aba9b0
Carlos <carlospelinski03@gmail.com>
2025-06-17 10:00:34 -0300

    Add business_image field to BusinessPayload for optional image uploads

--- server/app/validations/BusinessType.py
+++ server/app/validations/BusinessType.py
@@ -1,4 +1,5 @@
-from typing import TypedDict
+from typing import TypedDict, Optional
+from werkzeug.datastructures import FileStorage
 
 class BusinessPayload(TypedDict):
     business_name: str
@@ -9,3 +10,4 @@ class BusinessPayload(TypedDict):
     city: str
     business_phone: str
     business_email: str
+    business_image: Optional[FileStorage]
