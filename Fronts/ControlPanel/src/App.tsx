531d4999dae3d0105172b434bbe9e44c046e080c
Carlos <carlospelinski03@gmail.com>
2025-06-17 12:52:45 -0300

    Add MenuConfig route to App component for menu configuration navigation

--- Fronts/ControlPanel/src/App.tsx
+++ Fronts/ControlPanel/src/App.tsx
@@ -5,12 +5,14 @@ import Dashboard from "./Pages/Dashboard/Dashboard"
 import Auth from "./Pages/Auth/Auth"
 import Layout from "./components/Layout"
 import Business from "./Pages/Business/Business"
+import MenuConfig from "./Pages/MenuConfig/MenuConfig"
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout/>}>
         <Route index element={<Dashboard/>}/>
         <Route path="business" element={<Business/>}/>
+        <Route path="menu-configuration" element={<MenuConfig/>}/>
       </Route>
       <Route path="/authentication" element={<Auth/>}/>
     </Routes>
