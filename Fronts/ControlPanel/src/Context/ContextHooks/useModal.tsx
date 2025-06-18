e3839b8aedbf56cbb5fc1ded776ab790064802e9
Carlos <carlospelinski03@gmail.com>
2025-06-17 23:36:53 -0300

    Refactor useModal hook to initialize modals with default state and improve close functions

--- Fronts/ControlPanel/src/Context/ContextHooks/useModal.tsx
+++ Fronts/ControlPanel/src/Context/ContextHooks/useModal.tsx
@@ -1,18 +1,36 @@
 import { useMemo, useState } from 'react'
 import type { BusinessModal } from '../HookTypes/ModalTypes'
+import { type CategoriesModal } from '../HookTypes/Categories'
 
 function useModal() {
-    const [businessModal, setBusinessModal] = useState<BusinessModal | null>(null)
+  const [businessModal, setBusinessModal] = useState<BusinessModal>({
+    formType: "create",
+    opened: false
+  })
+  const [categoriesModal, setCategoriesModal] = useState<CategoriesModal>({
+    formType: "create",
+    opened: false
+  })
 
-    const closeBusinessModal = () => {
-        setBusinessModal(null)
-    }
+  const closeBusinessModal = () => {
+    setBusinessModal({
+      formType: "create",
+      opened: false
+    })
+  }
+
+  const closeCategoriesModal = () => {
+    setCategoriesModal({
+      formType: "create",
+      opened: false
+    })
+  }
   return useMemo(() => ({
-    businessModal, setBusinessModal,
-    closeBusinessModal
-  }),[
-    businessModal, setBusinessModal,
-    closeBusinessModal
+    businessModal, setBusinessModal, closeBusinessModal,
+    closeCategoriesModal, categoriesModal, setCategoriesModal
+  }), [
+    businessModal, setBusinessModal, closeBusinessModal,
+    closeCategoriesModal, categoriesModal, setCategoriesModal
   ])
 }
 
