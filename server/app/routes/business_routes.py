06e010b0803b95bc838abde4817d8cf912ccf89d
Carlos <carlospelinski03@gmail.com>
2025-06-19 16:54:12 -0300

    Modificar la ruta de creación de negocios para incluir validación de campos requeridos y ajustar la importación de servicios a la nueva estructura del proyecto, mejorando la gestión de datos y la organización del código.

--- server/app/routes/business_routes.py
+++ server/app/routes/business_routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request, jsonify
-from app.services.business_services import create_business, get_business_by_user
+from app.services.Business.business_services import create_business, get_business_by_user
 
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.validations.BusinessType import BusinessPayload
+
 business_routes = Blueprint('business_routes', __name__)
 
 @business_routes.route('/save', methods=['POST'])
@@ -11,7 +12,7 @@ def create_business_route():
     user_id = get_jwt_identity()
 
     try:
-        business_data: BusinessPayload = {
+        required_fields = {
             "business_name": request.form.get("business_name"),
             "business_description": request.form.get("business_description"),
             "business_address1": request.form.get("business_address1"),
@@ -20,7 +21,22 @@ def create_business_route():
             "city": request.form.get("city"),
             "business_phone": request.form.get("business_phone"),
             "business_email": request.form.get("business_email"),
-            "business_image": request.files.get("business_image") 
+        }
+        
+        for field_name, field_value in required_fields.items():
+            if field_value is None:
+                return jsonify({"message": f"Campo requerido faltante: {field_name}"}), 400
+        
+        business_data: BusinessPayload = {
+            "business_name": required_fields["business_name"],  # type: ignore
+            "business_description": required_fields["business_description"],  
+            "business_address1": required_fields["business_address1"],  
+            "countryCode": required_fields["countryCode"],  
+            "regionCode": required_fields["regionCode"],  
+            "city": required_fields["city"],  
+            "business_phone": required_fields["business_phone"],  
+            "business_email": required_fields["business_email"],  
+            "business_image": request.files.get("business_image")
         }
     except Exception as e:
         return jsonify({"message": f"Error al procesar los datos del formulario: {str(e)}"}), 400
